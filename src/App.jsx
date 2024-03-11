137e5eff37de2d3017889d4a89818c6e10c587b1
isaiful508 <isaiful.islam508@gmail.com>
2024-03-11 15:40:45 +0600

    done display country flags

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@
 import { useState } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
-import { useEffect } from 'react';
+
 
 function App() {
   
-const [countries, setCountries] = useState([]);
-
-useEffect (() => {
-  fetch('https://restcountries.com/v3.1/all')
-  .then(res => res.json())
-  .then(data => console.log(data))
-}, [])
 
 
 
